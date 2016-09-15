85953912ccdc6bb2a8a4ef91404a54af7d904e1a
Alec <email": null>
2016-09-15 13:13:10 -0700

    Add missle alerts

--- application.js
+++ application.js
@@ -1,12 +1,12 @@
-  game = 0
+time = 0
+game = 0
+
 function Missile() {
-  this.speed = Math.floor((Math.random() * 10) + 1);
+  this.speed = Math.floor((Math.random() * 10) + 3);
   this.area = Math.floor((Math.random() * 800) + 1);
 }
 
-
 rounds = Math.floor((Math.random() * 10));
-
 
 missiles = []
 for(var i=0; rounds > i; i++){
@@ -14,106 +14,102 @@ for(var i=0; rounds > i; i++){
   missiles.push(missile);
 }
 
-
-// console.log(missiles)
-
+console.log(missiles)
 
 function missileTimer(){
   if (game === 2000) { return; }
     game += 2
+    console.log(game)
+  if(game === 1430 && time === 0){
+    alert("crashed")
+  }
+
   setTimeout(missileTimer, 10);
   $(".missile").css("right", game);
 }
 
 function animation(id){
-  console.log(id)
-  $("#"+id).animate({"right": "+=2000px"}, 5000 * missiles[id].speed)
+  $("#"+id).animate({"right": "+=2500px"}, 700 * missiles[id].speed)
 }
 
-
-
-  // $("."+i).css({"right": game, "margin-top": missile.area});
+//   $("."+i).css({"right": game, "margin-top": missile.area});
 
 // for(var i=0; missiles.length > i; i++){
 
 
-    // $(".missile").after("<div class='missile'  + i + 'style='right:" + game + "px;margin-top:" + missiles[i].place + "px'></div>");
-    // $("#missile").after("<div class='missile " + i +"'style='right:" +  game + "px;margin-top:" + missiles[i].place + "px'></div>");
-    // $("."+i).css({"right": game, "margin-top": missile.place});
-  // }
+//     $(".missile").after("<div class='missile'  + i + 'style='right:" + game + "px;margin-top:" + missiles[i].place + "px'></div>");
+//     $("#missile").after("<div class='missile " + i +"'style='right:" +  game + "px;margin-top:" + missiles[i].place + "px'></div>");
+//     $("."+i).css({"right": game, "margin-top": missile.place});
+//   }
 // });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function timeBackward (){
-      if (click === false || time === 0) { return; }
-      time -= 3
-      setTimeout(timeBackward, 1);
-    $('#box').css('margin-top', time);
-    $('#box').css('-webkit-transform', 'rotate(-7deg)')
-   }
+  if (click === false || time === 0) { return; }
+  time -= 3
+  setTimeout(timeBackward, 1);
+  $('#box').css('margin-top', time);
+  $('#box').css('-webkit-transform', 'rotate(-7deg)')
+}
 
 function timeForward (){
-  if (unClick === false || time >= 700) { return; }
-    time += 3
-    setTimeout(timeForward, 1);
-    $('#box').css('margin-top', time);
-      if (time >= 700){
-        $('#box').css('-webkit-transform', 'rotate(0deg)');
-      } else {
-        $('#box').css('-webkit-transform', 'rotate(7deg)');
-      };
-   };
+  if (unClick === false || time >= window.innerHeight - 100 ) { return; }
+  time += 3
+  if(time === (window.innerHeight - 98)){
+    alert("crashed")
+  }
+  setTimeout(timeForward, 1);
+  $('#box').css('margin-top', time);
+  if (time >= window.innerHeight - 100 ){
+    $('#box').css('-webkit-transform', 'rotate(0deg)');
+  } else {
+    $('#box').css('-webkit-transform', 'rotate(7deg)');
+  };
+};
 
 $(document).ready(function(){
-
-  time = 0
   missileTimer();
 
-  // if(game != 10000){
-  //   // timer();
+  for (var i=0; i < missiles.length; i++){
+    $(".missile").after("<div class=rocket " + 'id='+ i + " style=margin-top:"+ i * 100 + "px;right:" +  -100 + "px;></div>");
+  }
 
-for (var i=0; i < missiles.length; i++){
-  $(".missile").after("<div class=rocket " + 'id='+ i + " style=margin-top:"+ i * 100 + "px;right:" +  -100 + "px;></div>");
-}
+  for (var i=0; i < missiles.length; i++){
+    animation(i);
+  }
 
-for (var i=0; i < missiles.length; i++){
-  animation(i);
-}
+  marginTop = 0
 
-  // }
 
-  $(window).mousedown(function(e){
-    e.preventDefault();
-    unClick = false;
-    click = true;
 
-    if(click === true){
+// add loop for missiles to check if they have reached max margin, if so, start back to zero and start animation method
+// for (var i=0; i < missiles.length; i++){
+//   if()
+// }
+//  if()
+
+  while((window.innerHeight - 100) > marginTop){
+    $(window).mousedown(function(e){
+      e.preventDefault();
+      unClick = false;
+      click = true;
+
+      if(click === true){
         timeBackward();
       };
-  });
+    });
 
     $(window).mouseup(function(e){
-    e.preventDefault();
-    click = false;
-    unClick = true;
-
-    if(unClick === true){
-      timeForward();
-    };
-  });
+      e.preventDefault();
+      click = false;
+      unClick = true;
+      if(unClick === true){
+        timeForward();
+      };
+    });
+    element = document.getElementById("box");
+    style = window.getComputedStyle(element);
+    marginTop = style.getPropertyValue("margin-top")
+  }
 });
